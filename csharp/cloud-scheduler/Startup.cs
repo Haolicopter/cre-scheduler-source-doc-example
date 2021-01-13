01f211acbed925bf42d95b9ee4ccba55a3d6d222
Hao <linghaoo@gmail.com>
2021-01-13 13:31:42 -0500

    C# receiver respond with a cloudevent instead of empty response

--- csharp/cloud-scheduler/Startup.cs
+++ csharp/cloud-scheduler/Startup.cs
@@ -14,15 +14,18 @@
 
 // [START event_handler]
 
+using System;
+using System.Net.Mime;
+using CloudNative.CloudEvents;
+using Google.Events;
+using Google.Events.Protobuf.Cloud.Scheduler.V1;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using CloudNative.CloudEvents;
-using Google.Events;
-using Google.Events.Protobuf.Cloud.Scheduler.V1;
+using Newtonsoft.Json;
 
 public class Startup
 {
@@ -47,23 +50,36 @@ public class Startup
             {
                 logger.LogInformation("Handling HTTP POST");
 
-                var ceId = context.Request.Headers["ce-id"];
-                var ceTime = context.Request.Headers["ce-time"];
-                logger.LogInformation($"ce-id: {ceId}");
-
                 var cloudEvent = await context.Request.ReadCloudEventAsync();
-                var data = CloudEventConverters.ConvertCloudEventData<SchedulerJobData>(cloudEvent);
-                logger.LogInformation($"Scheduler data: {data.CustomData.ToBase64()}");
-
-                if (string.IsNullOrEmpty(ceId))
+                if (string.IsNullOrEmpty(cloudEvent.Id))
                 {
-                    context.Response.StatusCode = 400;
+                    context.Response.StatusCode = StatusCodes.Status400BadRequest;
                     await context.Response.WriteAsync("Bad Request: expected header ce-id");
                     return;
                 }
+                logger.LogInformation($"Cloud Scheduler executed a job (id: {cloudEvent.Id}) at {cloudEvent.Time}");
+                var schedulerJobData = CloudEventConverters.ConvertCloudEventData<SchedulerJobData>(cloudEvent);
+                logger.LogInformation($"Scheduler data: {schedulerJobData.CustomData.ToBase64()}");
 
-                logger.LogInformation($"Cloud Scheduler executed a job (id: {ceId}) at {ceTime}");
-                await context.Response.WriteAsync("");
+                // reply with a cloudevent
+                var replyEvent = new CloudEvent(
+                    CloudEventsSpecVersion.V1_0, 
+                    "com.example.kuberun.events.received",
+                    new Uri("https://localhost"), 
+                    Guid.NewGuid().ToString(),
+                    DateTime.Now)
+                {
+                    DataContentType = new ContentType(MediaTypeNames.Application.Json),
+                    Data = JsonConvert.SerializeObject("Event received")
+                };
+
+                context.Response.StatusCode = StatusCodes.Status200OK;
+                context.Response.Headers.Add("Ce-Id", replyEvent.Id);
+                context.Response.Headers.Add("Ce-Specversion", "1.0");
+                context.Response.Headers.Add("Ce-Type", replyEvent.Type);
+                context.Response.Headers.Add("Ce-Source", replyEvent.Source.ToString());
+                context.Response.ContentType = MediaTypeNames.Application.Json;
+                await context.Response.WriteAsync(replyEvent.Data.ToString());
             });
         });
     }
